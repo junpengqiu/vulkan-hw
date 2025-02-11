21b69a28b126a0daec70f0076dc23fd02dc85bbe
Junpeng Qiu <naminoqiu@gmail.com>
2025-02-12 01:41:51 +0800

    enabled vk portability subset ext for macOS

--- main.cpp
+++ main.cpp
@@ -189,6 +189,10 @@ private:
         createInfo.pApplicationInfo = &appInfo;
 
         auto extensions = getRequiredExtensions();
+#ifdef __APPLE__
+        extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif
         createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
         createInfo.ppEnabledExtensionNames = extensions.data();
 
